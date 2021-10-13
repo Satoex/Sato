98852a32636a54f1b0ce8ebd1433570601fcc2b8
Satoex <78755872+Satoex@users.noreply.github.com>
2021-10-13 18:32:30 +0200

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -24,7 +24,7 @@ static const CAmount CENT = 1000000;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 30000000000 * COIN;
+static const CAmount MAX_MONEY = 8000000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  SATO_AMOUNT_H
